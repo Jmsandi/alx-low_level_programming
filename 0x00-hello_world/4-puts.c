a579e2c928c0b7e38ec9ea1c18ed96bb54ba5d3b
Jmsandi <Juliusmsandi@gmail.com>
2023-03-18 07:07:15 -0700

    hello c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: 0 after successful printing.
+ * Return: Always 0.
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
- return (0); }
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+0x00. C - Hello, World
+18/03/2023, 7:14 AM
+Page 1 of 4
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+}
